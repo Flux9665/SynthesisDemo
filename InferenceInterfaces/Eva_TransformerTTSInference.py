49e9b8752a375a1cd599761339049e54f21e2c36
Florian Lux <florian.lux@ims.uni-stuttgart.de>
2021-04-21 13:22:07 +0200

    fix turning tensor into numpy array prematurely

--- InferenceInterfaces/Eva_TransformerTTSInference.py
+++ InferenceInterfaces/Eva_TransformerTTSInference.py
@@ -318,7 +318,7 @@ class Eva_TransformerTTSInference(torch.nn.Module):
             mel = self.phone2mel(phones, speaker_embedding=self.speaker_embedding).transpose(0, 1)
             wave = self.mel2wav(mel.unsqueeze(0)).squeeze(0).squeeze(0)
             if jupyter:
-                wave = torch.cat((wave.cpu(), torch.zeros([8000])), 0).numpy()
+                wave = torch.cat((wave.cpu(), torch.zeros([8000])), 0)
         if view:
             import matplotlib.pyplot as plt
             import librosa.display as lbd
@@ -331,39 +331,5 @@ class Eva_TransformerTTSInference(torch.nn.Module):
             plt.subplots_adjust(left=0.05, bottom=0.1, right=0.95, top=.9, wspace=0.0, hspace=0.0)
             plt.show()
 
-        return wave
-
-    def read_to_file(self, text_list, file_location, silent=False):
-        """
-        :param silent: Whether to be verbose about the process
-        :param text_list: A list of strings to be read
-        :param file_location: The path and name of the file it should be saved to
-        """
-        wav = None
-        silence = torch.zeros([8000])
-        for text in text_list:
-            if text.strip() != "":
-                if not silent:
-                    print("Now synthesizing: {}".format(text))
-                if wav is None:
-                    wav = self(text).cpu()
-                    wav = torch.cat((wav, silence), 0)
-                else:
-                    wav = torch.cat((wav, self(text).cpu()), 0)
-                    wav = torch.cat((wav, silence), 0)
-        soundfile.write(file=file_location, data=wav.cpu().numpy(), samplerate=16000)
-
-    def read_aloud(self, text, view=False, blocking=False):
-        if text.strip() == "":
-            return
-
-        wav = self(text, view).cpu()
-        wav = torch.cat((wav, torch.zeros([8000])), 0)
-
-        if not blocking:
-            sounddevice.play(wav.numpy(), samplerate=16000)
-
-        else:
-            sounddevice.play(torch.cat((wav, torch.zeros([12000])), 0).numpy(), samplerate=16000)
-            sounddevice.wait()
+        return wave.numpy()
 
